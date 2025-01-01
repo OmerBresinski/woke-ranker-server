eab140e8613c249a23d635d4f127d7afede8311b
Omer Bresinski <omerbres@gmail.com>
2025-01-01 17:50:26 +0000

    prompt

--- src/utils.ts
+++ src/utils.ts
@@ -17,7 +17,7 @@ export const queryWokenessFromGrok = async (
           In addition, as a golden rule, if the movie mocks wokeness or makes fun of leftist values, it is not woke and should be scored a 1.
           If a movie is a spinoff, sequel, or prequel, and the characters are now diverse, gender swapped, or the plot is changed to fit a woke narrative, it is woke and should be scored a 5.
           If the movie name is Conclave, it's woke and should be scored a 4 with a matching summary.
-          Based on the following number: ${wokeMeter}, which is a score between 1 and 3 of how polite your 'summary' response would be. If the score is 1, the summary should be not polite at all and make a complete fun of the movie and make a mockery of it, with the intention to embarass the film's directors, and it's casting choices, as well as how blatantly the directors sacrificed the plot for wokeness, use harsh words and mock the film directors as much as you can. A score of 3 should be very polite, and detailed. **THE WOKE METER IS NOT THE WOKE SCORE, GIVE THE WOKE SCORE REGARDLESS OF THE WOKE METER LEVEL**
+          Based on the following number: ${wokeMeter}, which is a score between 1 and 3 of how polite your 'summary' response would be. If the score is 1, the summary should be not polite at all and make a complete fun of the movie and make a mockery of it, with the intention to embarass the film's directors, and it's casting choices, as well as how blatantly the directors sacrificed the plot for wokeness, use the directors' names if possible, use harsh words and mock the film directors as much as you can, you can refer to some plotpoints but DO NOT SPOIL THE MOVIE NO MATTER WHAT. A score of 3 should be very polite, and detailed. **THE WOKE METER IS NOT THE WOKE SCORE, GIVE THE WOKE SCORE REGARDLESS OF THE WOKE METER LEVEL**
           Do not spoil the movie or even hint at any spoilers.
           If a movie name has typos, or not a full name, for example: hary poter 5 instead of Harry Potter and the Order of the Phoenix, you should still be able to figure it out and adjust the name to the correct one.
   
@@ -28,7 +28,7 @@ export const queryWokenessFromGrok = async (
                 headline: string;
             }
             
-            A response would be composed of a movieName, a wokeScore which describes how woke the movie is on a scale of 1-5 based on the summary, the summary of why that score was given (with politeness based on the wokeMeter level) max 100 words, and a headline which would be two short sentences describing how woke or not woke the movie is in an intellectual playful manner while referencing something from the film, starting with something like "Ah, you're looking for" or something similar. Make sure that the wokeScore matches the summary.
+            A response would be composed of a movieName, a wokeScore which describes how woke the movie is on a scale of 1-5 based on the summary, the summary of why that score was given (with politeness based on the wokeMeter level) max 100 words, and a headline which would be two short sentences describing how woke or not woke the movie is in an intellectual playful manner while referencing something from the film, starting with something similar to "Ah, you're looking for", but not always that. Make sure that the wokeScore matches the summary.
   
             If the movie does not exist on IMDBTV, return null in all of the fields
             
